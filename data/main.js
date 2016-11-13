667095800d5b6d966290ccfa8ea997a011f7019d
Victor Permild <victor@Victors-MacBook-Pro.local>
2016-11-13 18:04:49 +0100

    Victor stuff works

--- data/main.js
+++ data/main.js
@@ -1,93 +1,125 @@
 $(document).ready(function() {
-    $('.navigation-item').on('click', function() {
-        var navId = $(this).attr('id');
-        $('.shown').removeClass('shown');
-        $('.tool-container[data-name=' + navId + ']').addClass('shown');
-    });
+	$('.navigation-item').on('click', function() {
+		var navId = $(this).attr('id');
+		$('.shown').removeClass('shown');
+		$('.tool-container[data-name=' + navId + ']').addClass('shown');
+	});
 
-    $('#switch-submit').on('click', function() {
-        createSwitch();
-    });
+	$('#switch-submit').on('click', function() {
+		createSwitch();
+	});
 
-    $('.add-parameter').on('click', function() {
-        var parameterClone = $('.parameter-item:first').clone();
-        parameterClone.find('input').val('');
-        $('.parameter-container').append(parameterClone);
-    });
+	$('.add-parameter').on('click', function() {
+		var parameterClone = $('.parameter-item:first').clone();
+		parameterClone.find('input').val('');
+		$('.parameter-container').append(parameterClone);
+	});
 
-    $('#return-parameter').on('change', function() {
-        if($(this).is(':checked')) {
-            $('.return-container').show();
-        } else {
-            $('.return-container').hide();
-        }
-    });
+	$('#return-parameter').on('change', function() {
+		if ($(this).is(':checked')) {
+			$('.return-container').show();
+		} else {
+			$('.return-container').hide();
+		}
+	});
 
-    $('#documentation-submit').on('click', function() {
-        generateDocumentation();
-    });
+	$('#activate-label').on('change', function() {
+		if ($(this).is(':checked')) {
+			$('.label-container').show();
+		} else {
+			$('.label-container').hide();
+		}
+	});
 
-    $('.error-text').text('app is: ' + app).show();
+	$('#documentation-submit').on('click', function() {
+		generateDocumentation();
+	});
+	$('#documentation-submit').on('click', function() {
+		disableCode();
+	});
+
+	$('.error-text').text('app is: ' + app).show();
+
 
 });
 
 function confirmReception() {
-    $('.switch-error').text('Sending to Processing...').show();
+	$('.switch-error').text('Sending to Processing...').show();
 }
 
 function createSwitch() {
-    $('#switch-error').text('WE CALLED IT').show();
-    var noOfStatements = $('#no-of-cases').val();
-    var varName = $('#name-of-variable').val();
-    if(noOfStatements.length) {
-        if(varName) {
-            $('#switch-error').hide();
-            $('#switch-error').text('Sending to Processing...').show();
-            app.generateSwitch(noOfStatements, varName);
-        }
-        else {
-            $('#switch-error').text('Please give your variable a name').show();
-        }
-    } else {
-        $('#switch-error').text('Please write a number').show();
-    }
+	$('#switch-error').text('WE CALLED IT').show();
+	var noOfStatements = $('#no-of-cases').val();
+	var varName = $('#name-of-variable').val();
+	if (noOfStatements.length) {
+		if (varName) {
+			$('#switch-error').hide();
+			$('#switch-error').text('Sending to Processing...').show();
+			app.generateSwitch(noOfStatements, varName);
+		} else {
+			$('#switch-error').text('Please give your variable a name').show();
+		}
+	} else {
+		$('#switch-error').text('Please write a number').show();
+	}
 }
 
 function generateDocumentation() {
-    var methodDescription = $('#documentation-description').val();
+	var methodDescription = $('#documentation-description').val();
 
-    var documentationString =
-        '/**\n'
-        + ' * ' + methodDescription + '\n'
-        + ' *\n';
+	var documentationString = '/**\n' + ' * ' + methodDescription + '\n'
+			+ ' *\n';
 
-    var parameterArr = [];
-    $('.parameter-item').each(function(index, elem) {
-        var name = $(elem).find('#parameter-name').val();
-        var description = $(elem).find('#parameter-description').val();
+	var parameterArr = [];
+	$('.parameter-item').each(
+			function(index, elem) {
+				var name = $(elem).find('#parameter-name').val();
+				var description = $(elem).find('#parameter-description').val();
 
-        var item = {
-            name : name,
-            description : description
-        };
+				var item = {
+					name : name,
+					description : description
+				};
 
-        parameterArr.push(item);
+				parameterArr.push(item);
 
-        if(name && description) {
-            documentationString += ' * @param ' + name + ' ' + description + '\n';
-        }
-    });
+				if (name && description) {
+					documentationString += ' * @param ' + name + ' '
+							+ description + '\n';
+				}
+			});
 
-    var returnDescription;
-    if( $('#return-parameter').is(':checked') ) {
-        returnDescription = $('#return-description').val();
+	var returnDescription;
+	if ($('#return-parameter').is(':checked')) {
+		returnDescription = $('#return-description').val();
 
-        documentationString += ' * @return ' + returnDescription + '\n';
-    }
+		documentationString += ' * @return ' + returnDescription + '\n';
+	}
 
-    documentationString += ' */\n';
+	documentationString += ' */\n';
 
-    $('#documentation-error').html(documentationString).show();
+	$('#documentation-error').html(documentationString).show();
 
-    app.insertDocumentation(documentationString);
+	app.insertDocumentation(documentationString);
+}
+
+function disableCode(){
+	var labelWanted, timeWanted, reason;
+	if($('#activate-label').is(':checked')){
+		labelWanted = true;
+		
+	} else{
+		labelWanted = false;
+	}
+	
+	if($('#time-stamp').is(':checked')){
+		timeWanted = true;
+		
+	} else{
+		timeWanted = false;
+	}
+	reason = $('#comment-description').val();
+	
+	app.disableCode(labelWanted, timeWanted, reason);
+	
 }
